4895610a37e9cf6887b9ab788947426b4cc17db5
sadharuuu <165069038+sadharuuu@users.noreply.github.com>
2026-05-25 01:06:14 +0530

    cookie solved

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -48,7 +48,7 @@ const login=async(req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             secure:true,
-            sameSite:none,
+            sameSite:"none",
             maxAge:60*60*1000           
         })
         res.status(200).json({
